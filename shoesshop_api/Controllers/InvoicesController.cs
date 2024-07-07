1d9a6039810a773db819c04a11edacbec2cd23f8
datnt286 <datnt286@gmail.com>
2024-07-07 14:27:54 +0700

    add invoice orderby

--- shoesshop_api/Controllers/InvoicesController.cs
+++ shoesshop_api/Controllers/InvoicesController.cs
@@ -171,6 +171,7 @@ namespace shoesshop_api.Controllers
 				.Include(i => i.User)
 				.Include(i => i.InvoiceDetails)
 					.ThenInclude(id => id.Product)
+				.OrderByDescending(i => i.CreateDate)
 				.Skip((currentPage - 1) * pageSize)
 				.Take(pageSize)
 				.ToListAsync();
